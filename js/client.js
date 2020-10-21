8c9ad5ec69aaf20addb7fc324852fd2cb13481eb
Josh <joshrazonn@gmail.com>
2020-10-21 14:32:34 -0600

    made phrase generator tutorial

---
+++ js/client.js
@@ -0,0 +1,30 @@
+import { nouns } from './nouns.js';
+import { animals } from './animals.js';
+import { randUtil } from './random.js';
+
+const init = function(){
+  console.log(nouns);
+  console.log(animals);
+  console.log(randUtil);
+}
+
+
+function renderWords() {
+  // make container for first h1
+  const phrase = document.querySelector('#word1');
+  
+  // make container for first h1
+  const phrase2 = document.querySelector('#word2');
+  
+  // Change inner text to a random item on nouns array
+  const newPhrase = phrase.innerText = randUtil.randArrayItem(nouns);
+  const newPhrase2 = phrase2.innerText = randUtil.randArrayItem(nouns);
+}
+
+// Grabbing the button from html
+// Placing inside "btn" container
+const btn = document.querySelector('button');
+
+btn.addEventListener("click", renderWords);
+
+window.addEventListener('load', init);
